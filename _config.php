06432e73cbe4e4e690e0f7adea3d451b73f640df
sparkcom <ben@zenserve.eu>
2018-10-03 08:20:08 -0700

    load css/js from correct path

--- _config.php
+++ _config.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use SilverStripe\Core\Config\Config;
 
 //define global path to Components' root folder
@@ -7,5 +8,6 @@ if (!defined('BLOCKS_DIR')) {
     define('BLOCKS_DIR', rtrim(basename(dirname(__FILE__))));
 }
 
-Config::modify()->update('LeftAndMain', 'extra_requirements_javascript', array(BLOCKS_DIR.'/javascript/blocks-cms.js'));
-Config::modify()->update('BlockAdmin', 'menu_icon', BLOCKS_DIR.'/images/blocks.png');
+Config::modify()->update('SilverStripe\Admin\LeftAndMain', 'extra_requirements_javascript', array('vendor/sheadawson/silverstripe-blocks/client/javascript/blocks-cms.js'));
+Config::modify()->update('SheaDawson\Blocks\Controllers\BlockAdmin', 'menu_icon_class', 'font-icon-block-banner');
+
